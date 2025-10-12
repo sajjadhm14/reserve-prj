8435793c9af093ecfc9090b58d0ed73bf1d8311d
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-12 16:19:39 +0330

    make admin partial to add navbar/footer/sidebar

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,29 @@
+<nav class="sidebar">
+    <div class="sidebar-header">
+        <a href="{{route('dashboard')}}" class="sidebar-brand">
+            HM-<span>SAJJAD</span>
+        </a>
+        <div class="sidebar-toggler not-active">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+    <div class="sidebar-body">
+        <ul class="nav">
+            <!-- Main -->
+            <li class="nav-item nav-category">Main</li>
+            <li class="nav-item">
+                <a href="{{route('dashboard')}}" class="nav-link">
+                    <i class="link-icon" data-feather="box"></i>
+                    <span class="link-title">Dashboard</span>
+                </a>
+            </li>
+
+            <!-- Components -->
+
+
+        </ul>
+    </div>
+</nav>
