178d3ff152748a09b258ddf4693d7bfbdd5bc521
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-15 18:05:17 +0330

    modify bootstrap file to add middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'consulter' => \App\Http\Middleware\Consulter::class
+            'consulter_middleware' => \App\Http\Middleware\Consulter::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
