0048de611c5d3793cfad69f21172ebd32af50a5c
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-15 15:17:55 +0330

    modify reservation controller to add store method

--- app/Http/Controllers/backend/ReservationController.php
+++ app/Http/Controllers/backend/ReservationController.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReservationController extends Controller
 {
     public function index(){
         return view('admin.pages.reservation-index');
+    }
+
+    public function store(Request $request)
+    {
+        $request ->validate([
+          'consultant_id' => 'required',
+          'date' => 'required|date|after_or_equal:today',
+        ]);
+
+//      i want to check if time exicts or not ?
+
+        $exists = Reservation::where('consulter_id' , $request->consulter_id)
+//                                ->where('date' , $request->date)
+                                ->exists();
+
+
+        if($exists){
+            return response()->json([
+                'message' => 'consulter is fulled sorry',
+            ]);
+        }
+
+        $reservation = Reservation::create([
+            'user_id' =>Auth::id(),
+            'consultant_id' => $request->consulter_id,
+//            'date' => $request->date,
+        ]);
+
+        return response()->json([
+            'message' => 'your time is reserved successfully',
+            'data' =>$reservation,
+        ]);
+
     }
 }
