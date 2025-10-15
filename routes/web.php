19f42c1a0bc0785483119783ac26d5f8590aa9a9
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-15 18:01:25 +0330

    make route is needed

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,35 @@
 
 use App\Http\Controllers\backend\AdminController;
 use App\Http\Controllers\backend\ReservationController;
+use App\Http\Controllers\Consulter\ConsulterController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
 });
+
+
+Route::get('/consulter/login', [\App\Http\Controllers\Consulter\auth\ConsulterLoginController::class, 'showLoginForm'])->name('consulter.login');
+Route::post('/consulter/login', [\App\Http\Controllers\Consulter\auth\ConsulterLoginController::class, 'login']);
+
+
+Route::get('register-consulter' , [\App\Http\Controllers\Consulter\auth\RegisterController::class , 'create'])->name('consulter.register');
+Route::post('register-consulter' , [\App\Http\Controllers\Consulter\auth\RegisterController::class , 'register']);
+
+Route::middleware('consulter_middleware')->group(function () {
+
+    Route::get('consulter/index', [ConsulterController::class, 'index'])->name('consulter.index');
+});
+
+
+
+
+
+
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', function () {
@@ -18,8 +41,12 @@ Route::middleware('auth')->group(function () {
     Route::get('info' , [AdminController::class , 'info'])->name('info');
 
 //    reservation route
-
     Route::get('reservation' , [ReservationController::class , 'index'])->name('reservation.index');
+    Route::post('reservation-store' , [ReservationController::class , 'store'])->name('reservation.store');
+
+//    consultant route
+    Route::get('consulter' , [ConsulterController::class , 'index'])->name('consulter.index');
+    Route::post('consulter' , [ConsulterController::class , 'store'])->name('consulter.store');
 
 });
 
