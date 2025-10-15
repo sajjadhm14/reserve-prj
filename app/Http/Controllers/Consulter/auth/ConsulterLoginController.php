375fb22a9f81130b5e2d2c2614324187192a56ae
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-15 18:03:20 +0330

    modify consulter login controller

--- app/Http/Controllers/Consulter/auth/ConsulterLoginController.php
+++ app/Http/Controllers/Consulter/auth/ConsulterLoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Consulter\auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ConsulterLoginRequest;
 use Illuminate\Http\Request;
 
 class ConsulterLoginController extends Controller
@@ -12,9 +13,14 @@ class ConsulterLoginController extends Controller
         return view('consulter.auth.consulter-login');
     }
 
-    public function login(Request $request)
+    public function login(ConsulterLoginRequest $request)
     {
 
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect()->route('consulter.index');
     }
 
 }
