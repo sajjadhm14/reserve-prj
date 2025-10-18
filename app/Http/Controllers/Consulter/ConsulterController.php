52a2e1d16c076093c828d5e753bcec0e778b5a91
Sajjadhm14 <sajjadhamidi499@gmail.com>
2025-10-18 14:17:36 +0330

    modify consulter controller

--- app/Http/Controllers/Consulter/ConsulterController.php
+++ app/Http/Controllers/Consulter/ConsulterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Consulter;
 
 use App\Http\Controllers\Controller;
 use App\Models\consulter;
+use App\Models\Reservation;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -22,11 +23,27 @@ class ConsulterController extends Controller
 
     public function reservationCheck()
     {
-        return view ('consulter.pages.reservation-check');
+        $auth = auth('consulter')->id();
+
+        $reservations = Reservation::where('consulter_id', $auth)
+            ->with('user')
+            ->get();
+
+        return view ('consulter.pages.reservation-check' , compact('reservations'));
     }
 
     public function ConsulterInfo()
     {
         return view ('consulter.pages.info');
+    }
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('consulter/login');
     }
 }
